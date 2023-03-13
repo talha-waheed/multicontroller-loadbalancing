54775ef4e9828e3e571ebdb373f6c73a9e4f3a85
Talha Waheed <talhawaheed57@gmail.com>
2023-03-13 13:56:16 -0500

    updated. everything added except topology

--- go_server/main.go
+++ go_server/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"math"
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 )
 
 func processRequest(totalLoopCount, base, exp float64) float64 {
@@ -52,7 +54,7 @@ func respondWithSuccess(w http.ResponseWriter, loopCount string, base string, ex
 	fmt.Fprintf(w, "Processed at %s w/ loopCount=%s & compute=(%s,%s) => %f", getHostName(), loopCount, base, exp, reqResult)
 }
 
-func handleRoute(w http.ResponseWriter, r *http.Request) {
+func handleRequest(chIncrementNumOfReqs chan bool, w http.ResponseWriter, r *http.Request) {
 	loopCount := r.URL.Query().Get("loopCount")
 	base := r.URL.Query().Get("base")
 	exp := r.URL.Query().Get("exp")
@@ -70,12 +72,180 @@ func handleRoute(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	port := 3000
+	portToListenOn := 3000
+	chIncrementNumOfReqs := make(chan bool)
+	chGetAndFlushNumOfReqs := make(chan chan int)
+	centralControllerURL := getCentralControllerURL()
+	notifTimeInterval := 1 * time.Second
 
-	http.HandleFunc("/", handleRoute)
-	fmt.Printf("Server running (port=%d), route: http://localhost:%d/?loopCount=1&base=8&exp=7.7\n", port, port)
+	go manageNumOfReqs(chIncrementNumOfReqs, chGetAndFlushNumOfReqs)
 
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
+	go periodicallyNotifyCentralController(notifTimeInterval, chGetAndFlushNumOfReqs, centralControllerURL)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handleRequest(chIncrementNumOfReqs, w, r)
+	})
+	fmt.Printf("Server running (port=%d), route: http://localhost:%d/?loopCount=1&base=8&exp=7.7\n", portToListenOn, portToListenOn)
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", portToListenOn), nil); err != nil {
 		log.Fatal(err)
 	}
 }
+
+func manageNumOfReqs(chIncrementNumOfReqs chan bool, chGetAndFlushNumOfReqs chan chan int) {
+
+	numOfReqs := 0
+
+	for {
+		select {
+		case <-chIncrementNumOfReqs:
+			numOfReqs++
+		case chReply := <-chGetAndFlushNumOfReqs:
+			chReply <- numOfReqs
+			numOfReqs = 0
+		}
+	}
+}
+
+type Response struct {
+	ReqNum      int
+	IsError     bool
+	ErrMsg      string
+	StatusCode  int
+	Body        string
+	StartTimeNs int64
+	LatencyNs   int64
+	ReadTimeNs  int64
+}
+
+func getWaitDuration(notifTimeIntervalNs time.Duration) time.Duration {
+
+	currentTimeNs := time.Now().Nanosecond()
+	intervalNs := notifTimeIntervalNs.Nanoseconds()
+
+	waitIntervalNs := intervalNs - (int64(currentTimeNs) % intervalNs)
+
+	return time.Duration(waitIntervalNs)
+}
+
+func periodicallyNotifyCentralController(notifTimeInterval time.Duration, chGetAndFlushNumOfReqs chan chan int, centralControllerURL string) {
+
+	// wait for a whole k interval of time
+	waitDuration := getWaitDuration(notifTimeInterval)
+	time.Sleep(waitDuration)
+
+	// then after every k interval of time
+	// reliably send state to the central controller
+	repeatInterval := time.Duration(notifTimeInterval)
+	repeatTicker := time.NewTicker(repeatInterval)
+	chResponse := make(chan Response)
+	chGetNumOfReqs := make(chan int)
+
+	for {
+		select {
+		case <-repeatTicker.C:
+			reliablySendState(chGetAndFlushNumOfReqs, centralControllerURL, chResponse, chGetNumOfReqs)
+		}
+	}
+}
+
+type CentralControllerState struct {
+	podname string
+	k       int
+	a       int
+}
+
+// syncronous
+func sendStateToCentralController(
+	reqURL string,
+	podname string,
+	k int,
+	a int,
+	tryNum int) Response {
+
+	log.Printf("sending state [%s, %d, %d] to %s (try %d)", podname, k, a, reqURL, tryNum)
+
+	req, err := http.NewRequest(http.MethodGet, reqURL, nil)
+	if err != nil {
+		errMsg := fmt.Sprintf("client: could not create request: %s", err)
+		return Response{tryNum,
+			true, errMsg,
+			0, "",
+			time.Now().UnixNano(), 0, 0}
+	}
+	req.Header.Set("Connection", "close")
+
+	q := req.URL.Query()
+	q.Add("podname", podname)
+	q.Add("k", fmt.Sprintf("%d", k))
+	q.Add("a", fmt.Sprintf("%d", a))
+	req.URL.RawQuery = q.Encode()
+
+	startReq := time.Now()
+	res, err := http.DefaultClient.Do(req)
+	latency := time.Since(startReq)
+
+	if err != nil {
+		errMsg := fmt.Sprintf("client: error making http request: %s", err)
+		return Response{tryNum,
+			true, errMsg,
+			0, "",
+			startReq.UnixNano(), latency.Nanoseconds(), 0}
+	}
+
+	startRead := time.Now()
+	resBody, err := io.ReadAll(res.Body)
+	readTime := time.Since(startRead)
+
+	if err != nil {
+		errMsg := fmt.Sprintf("client: could not read response body: %s", err)
+		return Response{tryNum,
+			true, errMsg,
+			res.StatusCode, "",
+			startReq.UnixNano(), latency.Nanoseconds(), readTime.Nanoseconds()}
+	}
+
+	return Response{tryNum,
+		false, "",
+		res.StatusCode, string(resBody),
+		startReq.UnixNano(), latency.Nanoseconds(), readTime.Nanoseconds()}
+}
+
+func getAndFlushNumOfReqs(chGetAndFlushNumOfReqs chan chan int, chGetNumOfReqs chan int) int {
+	chGetAndFlushNumOfReqs <- chGetNumOfReqs
+	numOfReqs := <-chGetNumOfReqs
+	return numOfReqs
+}
+
+// synchronous
+func reliablySendState(chGetAndFlushNumOfReqs chan chan int, centralControllerURL string, chResponse chan Response, chGetNumOfReqs chan int) {
+
+	tryNum := 1
+	podname, err := os.Hostname()
+	if err != nil {
+		log.Printf("Error: couldn't look up the hostname of pod\n")
+	}
+	numOfReqs := getAndFlushNumOfReqs(chGetAndFlushNumOfReqs, chGetNumOfReqs)
+	currentTime := time.Now().Nanosecond()
+
+	for {
+		resp := sendStateToCentralController(centralControllerURL, podname, currentTime, numOfReqs, tryNum)
+
+		log.Printf("Resonse from CC for try %d: %d [%d]: %s [%s], latency: %fms",
+			resp.StatusCode, resp.Body, resp.ErrMsg, float64(resp.LatencyNs)/1000000)
+
+		if resp.StatusCode == 200 {
+			break
+		}
+
+		if tryNum >= 3 {
+			log.Printf("Error: no 200 response from CC in 3 tries. Stopping sending messages for k=%dns\n", currentTime)
+		}
+		tryNum++
+	}
+}
+
+/*
+PROBLEMS:
+-
+*/
